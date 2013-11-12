116205a0b591e7956e16b77741e965b44252019e
Julien Pottier <julien.pottier@isics.fr>
2013-11-12 17:00:30 +0100

    [NEW] Ajout de la tâche d'envoi des sondes

---
+++ project/modules/public/stable/iconito/statistiques/actiongroups/task.actiongroup.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright CAP-TIC
+ * @link      http://www.cap-tic.fr
+ */
+_classInclude("kernel|Tools");
+
+class ActionGroupTask extends CopixActionGroup
+{
+  public function processSendStatistiques()
+  {
+
+
+    return _arNone();
+  }
+}
