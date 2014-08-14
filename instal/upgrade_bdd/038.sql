4db52c2aaea528ce499075f863601fd3cfe9ebda
Philippe ROSER <proser@cap-tic.fr>
2014-08-14 10:54:52 +0200

    [FIX] Statistiques : Activation de la configuration

---
+++ instal/upgrade_bdd/038.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `copixmodule` (`name_cpm`, `path_cpm`, `version_cpm`) 
+VALUES ('activitystream', 'project/modules/public/stable/iconito/activitystream', '0'), 
+       ('statistiques', 'project/modules/public/stable/iconito/statistiques', '0');
