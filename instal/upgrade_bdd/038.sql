5aa7160570f061c4cdcb44140f851d94a7e8e5d7
Philippe ROSER <proser@cap-tic.fr>
2014-09-30 09:39:25 +0200

    [FIX] Statistiques : Suppression d'un patch SQL
    Ce patch oblige l’installation à avoir des statistiques. A ne mettre en
    place qu’en cas de configuration des Stats, cf. Wiki

--- instal/upgrade_bdd/038.sql
+++
@@ -1,3 +0,0 @@
-REPLACE INTO `copixmodule` (`name_cpm`, `path_cpm`, `version_cpm`) 
-VALUES ('activitystream', 'project/modules/public/stable/iconito/activitystream', '0'), 
-       ('statistiques', 'project/modules/public/stable/iconito/statistiques', '0');
